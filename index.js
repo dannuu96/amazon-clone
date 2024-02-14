71e471c880a2a2691200a3f291b48ade718c5ef3
Saud Khan <saudkhanbpk@gmail.com>
2024-02-14 20:52:15 +0500

    clg added

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+console.log("Danyal is our ameer sahib");
 
 AppRegistry.registerComponent(appName, () => App);
